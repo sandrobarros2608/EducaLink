2ed7ef78b5ece60dd23fc65907eec6561913b003
Sandro <sbarroscaballero0826@gmail.com>
2025-07-30 21:42:50 -0500

    Homework, Submission and Qualification deleted

--- src/main/java/com/company/educalink/repository/HomeworkRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.educalink.repository;
-
-import com.company.educalink.entity.Homework;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HomeworkRepository extends JpaRepository<Homework, Long> {
-}
--- src/main/java/com/company/educalink/repository/SubmissionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.educalink.repository;
-
-import com.company.educalink.entity.Submission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubmissionRepository extends JpaRepository<Submission, Long> {
-}
